01d34ed8d53c1a4d36200ddabbf7be848073fc88
Eirikfin <148289492+Eirikfin@users.noreply.github.com>
2025-04-04 13:47:48 +0200

    crud for users complete (for now)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from typing import Union
 from sqlalchemy.orm import Session
-from controllers.users_controller import createUser, updateUser
+from controllers.users_controller import createUser, updateUser, deleteUser, getUser, getAllUsers
 from schemas import UserCreate, UserUpdate
 from models import User
 from config.db import SessionLocal
@@ -21,10 +21,24 @@ def get_db():
 @app.post("/users")
 def register_user( req: UserCreate, db: Session = Depends(get_db)):
     return createUser(db, req)
-
+#updating a user:
 @app.patch("/users/{username}")
 def update_user(username: str, req: UserUpdate,  db: Session = Depends(get_db)):
     return updateUser(db, username, req)
+#delete a user:
+@app.delete("/user/{username}")
+def delete_user(username: str, db: Session = Depends(get_db)):
+    return deleteUser(db, username)
+#get a user:
+@app.get("/user/{username}")
+def get_user(username: str, db: Session = Depends(get_db)):
+    return getUser(db, username)
+#get all users:
+@app.get("/users")
+def get_all_users(db: Session = Depends(get_db)):
+    return getAllUsers(db)
+
+
 
 
 @app.get("/")
