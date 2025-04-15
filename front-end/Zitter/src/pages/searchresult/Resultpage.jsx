327830e5b7c3893544b74f6099b4946a284da4d6
Eirikfin <148289492+Eirikfin@users.noreply.github.com>
2025-04-15 15:25:04 +0200

    setup for searchpage

---
+++ front-end/Zitter/src/pages/searchresult/Resultpage.jsx
@@ -0,0 +1,17 @@
+
+
+export default function ResultPage(){
+
+
+
+    return(
+        <>
+        <h1>This is where search results should be displayed:</h1>
+        {/* 
+           show accounts matching result:
+           Show tweets matching result:
+           Show tweet mathing hastag? (should this be different?):  
+            */}
+        </>
+    )
+}
