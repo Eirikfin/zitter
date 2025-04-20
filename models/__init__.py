37360055e82b905c9e0ad7db3b4546ee8385a551
Hanaka15 <34461088+Hanaka15@users.noreply.github.com>
2025-04-20 22:56:02 +0200

    fixforeirik2

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,5 @@ from .tweets_model import Tweet
 from .hashtag_model import Hashtag
 from .tweet_hashtag_model import TweetHashtag
 from .base import Base
+
+__all__ = ["User", "Tweet", "Hashtag", "TweetHashtag", "Base"]
