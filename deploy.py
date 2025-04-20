083dafa8a3700a5bfeba02f87c2ea75f735d060b
Eirikfin <148289492+Eirikfin@users.noreply.github.com>
2025-04-20 23:18:11 +0200

    fixed backend table creation

--- deploy.py
+++ deploy.py
@@ -1,5 +1,5 @@
-from config.db import engine
-from models import *
+from config.db import engine, Base
+from models import User, Tweet, Hashtag, TweetHashtag
 import logging
 
 # Set up logging to show SQLAlchemy SQL queries
@@ -13,8 +13,8 @@ try:
     # Attempt to create the table
     User.__table__.create(bind=engine)  # This should create the table
     Tweet.__table__.create(bind=engine)
-    TweetHashtag.__table__.create(bind=engine)
     Hashtag.__table__.create(bind=engine)
+
     print("✅ Tables created!")
 
     # Optionally, print the User model to verify it's imported correctly
@@ -23,4 +23,3 @@ try:
 except Exception as e:
     # If there’s an error during table creation, it will print here
     print(f"❌ Error creating tables: {str(e)}")
-
